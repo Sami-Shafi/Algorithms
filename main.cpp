cbe1d8a8f4d3a31d7fdca8cda92dced5730fffe8
Sami-Shafi <freelancersami123@gmail.com>
2025-08-22 22:12:13 +0600

    Reachability Checker in Map Grid

--- main.cpp
+++ main.cpp
@@ -1,63 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// You will be given an undirected graph as input. Then you will be given Q queries. For each query you will be given a node X. You need to print the nodes that are connected with X in descending order.
-
-// Note: If there is no node connected to X, then print -1.
+// You are given an NXM sized 2D matrix that represents a map of a building. Each cell represents a wall, a floor or a room. You will be given two rooms A and B. You need to tell if you can go from room  to  by passing through the floors. You can walk left, right, up, and down through the floor cells. You can't pass through walls.
 
 // Input Format
-// The first line will contain N and E, the number of nodes and the number of edges, respectively. The values of the nodes range from 0 to N-1.
-// Next E lines will contain two node values which means there is a connection between first node and second node.
-// The next line will contain Q.
-// The following Q lines will each contain X.
+// The first input line has two integers N and M: the height and width of the map.
+// Then there are N lines of M characters describing the map. Each character is .(floor), #(wall), A or B (rooms).
 
 // Output Format
-// Output the nodes that are connected with  in descending order.
+// Output YES if you can go from room  to , NO otherwise.
+
+// Sample Input 0
+// 5 8
+// ########
+// #.A#...#
+// #.##.#B#
+// #......#
+// ########
+
+// Sample Output 0
+// YES
+
+char grid[1005][1005];
+bool vis[1005][1005];
+int level[1005][1005];
+vector<pair<int,int>> mv = {{-1,0}, {1,0}, {0, -1}, {0, 1}};
+int n, m;
+
+bool valid(int i, int j) {
+    if (i<0 || i>=n || j<0 || j>=m) return false;
+    return true;
+}
+
+void reachabilityWithBFS(int si, int sj, int di, int dj) {
+    if(!valid(si, sj)) return;
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    vis[si][sj] = true;
+    level[si][sj] = 0;
+
+    while (!q.empty())
+    {
+        pair<int, int> fr = q.front();
+        q.pop();
+
+        // cout << "Parent -> " << fr.first << " " << fr.second << endl;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = fr.first + mv[i].first;
+            int cj = fr.second + mv[i].second;
+
+            if(valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] != '#')
+            {
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                level[ci][cj] = level[fr.first][fr.second] + 1;
+            }
+        }
+        
+    }
+    
+    if(level[di][dj] == -1){
+        cout << "NO";
+    }else {
+        cout << "YES";
+    }
+}
 
 int main() {
     
-    int n, e;
-    cin >> n >> e;
-    vector<int> adj_list[n];        
+    cin >> n >> m;
+    int si,sj,di,dj;
 
-    while (e--)
-    {
-        int i, j;
-        cin >> i >> j;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            {
+                char ourChar;
+                cin >> ourChar;
+                grid[i][j] = ourChar;
+                if(ourChar == 'A'){
+                    si = i;
+                    sj = j;
+                }else if(ourChar == 'B'){
+                    di = i;
+                    dj = j;
+                }
+            }
 
-        adj_list[i].push_back(j);
-        adj_list[j].push_back(i);
-    }
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    reachabilityWithBFS(si, sj, di, dj);
+    // cout << si << " " << sj << " |" << " " << di << " " << dj;
 
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << i << " -> ";
-
-    //     for (int x : adj_list[i])
-    //         cout << x << " ";
-    //     cout << endl;
-    // }
-
-    int q;
-    cin >> q;
-        
-    while (q--)
-    {
-        int x;
-        cin >> x;
-
-        vector<int> targetVector(adj_list[x]);
-        if(targetVector.size() == 0) {
-            cout << -1 << endl;
-            continue;
-        }
-        sort(targetVector.begin(), targetVector.end(), greater<int>());
-        for(int ans : targetVector)
-            cout << ans << " ";
-        cout << endl;
-    }
-    
-    
     return 0;
 }
