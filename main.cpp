74b6a866062d3b4ab35022cb48d401f1cca2f6dc
Sami-Shafi <freelancersami123@gmail.com>
2025-08-23 00:47:57 +0600

    Counting Apartments in 2d grid - 2

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// You are given an NXM sized 2D matrix that represents a map of a building. Each cell represents a wall or a room. The connected rooms are called apartments. Your task is to count the number of apartments in that building. You can walk left, right, up, and down through the room cells. You can't pass through walls.
+// You are given an NXM sized 2D matrix that represents a map of a building. Each cell represents a wall or a room. The connected rooms are called apartments. Your task is to count the number rooms in each of the apartments in that building. You can walk left, right, up, and down through the room cells. You can't pass through walls.
+
+// You need to print the count of the rooms in ascending order. If there are no apartments available in that building, then you should print 0.
 
 // Input Format
 // The first input line has two integers N and M: the height and width of the map.
 // Then there are N lines of M characters describing the map. Each character is either .(room) or #(wall).
 
 // Output Format
-// Output the number of apartments
+// Output the number of rooms in each of the apartments in ascending order.
 
 // Sample Input 0
 // 5 8
@@ -19,7 +21,7 @@ using namespace std;
 // ########
 
 // Sample Output 0
-// 3
+// 2 2 8
 
 // Sample Input 1
 // 6 8
@@ -31,7 +33,7 @@ using namespace std;
 // #.#.##.#
 
 // Sample Output 1
-// 5
+// 1 1 2 8 10
 
 char grid[1005][1005];
 bool vis[1005][1005];
@@ -43,17 +45,18 @@ bool valid(int i, int j) {
     return true;
 }
 
-void dfs(int si, int sj) {
+void dfs(int si, int sj, int &roomsCt) {
     vis[si][sj] = true;
     for (int i = 0; i < 4; i++)
     {
         int ci = si + mv[i].first;
         int cj = sj + mv[i].second;
         
-        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
-            dfs(ci, cj);
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.'){
+            roomsCt++;
+            dfs(ci, cj, roomsCt);
+        }
     }
-    
 }
 
 int main() {
@@ -66,19 +69,29 @@ int main() {
             cin >> grid[i][j];
 
     int apartments = 0;
+    vector<int> rooms;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (!vis[i][j] && grid[i][j] == '.') {
                 apartments++;
-                dfs(i, j);
+                int roomsCt = 1;
+                dfs(i, j, roomsCt);
+                rooms.push_back(roomsCt);
             }
         }
     }
 
-    cout << apartments << endl;
+    if(apartments == 0){
+        cout << 0;
+    }else {
+        sort(rooms.begin(), rooms.end());
+        for (int roomCt : rooms)
+        {
+            cout << roomCt << " ";
+        }
+        
+    }
 
-    // memset(level, -1, sizeof(level));
-    // cout << si << " " << sj << " |" << " " << di << " " << dj;
 
     return 0;
 }
