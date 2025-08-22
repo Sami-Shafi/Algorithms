477514fa8d67717f16b03db740b4ee4cc5f3a7de
Sami-Shafi <freelancersami123@gmail.com>
2025-08-22 07:40:41 +0600

    2D Grid - DFS Traversal

--- main.cpp
+++ main.cpp
@@ -1,32 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj_list[1005];
-bool vis[1005];
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int,int>> mv = {{-1,0}, {1,0}, {0, -1}, {0, 1}};
+int n, m;
 
-void dfs(int src) {
-    cout << src << " ";
-    vis[src] = true;
+bool valid(int i, int j) {
+    if (i<0 || i>=n || j<0 || j>=m) return false;
+    return true;
+}
 
-    for (int child : adj_list[src])
-        if(!vis[child])
-            dfs(child);
+void dfs(int si, int sj){
+    // cout << "Parent Index: ";
+    cout << si << " " << sj << endl;
+    // cout << "-----------" << endl;
+    vis[si][sj] = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int childRow, childCol;
+        childRow = si + mv[i].first;
+        childCol = sj + mv[i].second;
+        // cout << "Child Index: " << childRow << " " << childCol << endl;
+        if(valid(childRow, childCol) && !vis[childRow][childCol])
+            dfs(childRow, childCol);
+    }
+    
 }
 
 int main() {
     
-    int n, e;
-    cin >> n >> e;
+    cin >> n >> m;
 
-    for (int i = 0; i < e; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        adj_list[a].push_back(b);
-        adj_list[b].push_back(a);
-    }
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> grid[i][j];
+    
+    
+    // print
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //         cout << grid[i][j] << " ";
+    //     cout << endl;
+    // }
+    
+    int srcRow, srcCol;
+    cin >> srcRow >> srcCol;
+
     memset(vis, false, sizeof(vis));
-    dfs(0);
+    dfs(srcRow, srcCol);
     
     return 0;
 }
