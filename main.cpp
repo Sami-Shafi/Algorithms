745cec23493a34ab2cbdec87021d0b099951a778
Sami-Shafi <freelancersami123@gmail.com>
2025-08-26 07:10:32 +0600

    Leetcode 695. Max Area of Island

--- main.cpp
+++ main.cpp
@@ -1,29 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 class Solution {
 public:
-    vector<vector<int>> adj_list;
-    vector<bool> vis;
+    int cnt, n, m;
+    int max = INT_MIN;
+    bool vis[55][55];
+    vector<pair<int,int>> mv = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
-    void dfs(int src) {
-        vis[src] = true;
-        for (int ch : adj_list[src])
-            if(!vis[ch])
-                dfs(ch);
+    bool valid(int i, int j) {
+        return !(i < 0 || i >= n || j < 0 || j >= m);
     }
 
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        adj_list.assign(n, {});
-        vis.assign(n, false);
-        for (int i = 0; i < edges.size(); i++){
-            int a = edges[i][0];
-            int b = edges[i][1];
-            adj_list[a].push_back(b);
-            adj_list[b].push_back(a);
-        }
+    void dfs(int si, int sj, vector<vector<int>>& grid) {
+        cnt++;
+        vis[si][sj] = true;
 
-        dfs(source);
-        return vis[destination];
+        for(pair mvPair : mv) {
+            int ci = si + mvPair.first;
+            int cj = sj + mvPair.second;
+
+            if (valid(ci, cj) && grid[ci][cj] == 1 && !vis[ci][cj])
+                {
+                    dfs(ci, cj, grid);
+                }
+        }
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        cnt = 0;
+        n = grid.size();
+        m = grid[0].size();
+        memset(vis, false, sizeof(vis));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(!vis[i][j] && grid[i][j] == 1)
+                    {
+                        dfs(i, j, grid);
+                        if(max < cnt) {
+                            max=cnt;
+                        }
+                        cnt=0;
+                    }
+
+        if (max <= 0) {
+            return 0;
+        }else {
+            return max;
+        }
     }
 };
