ece4cfb1fe6606f102242a10f163d0c82d5c9742
Sami-Shafi <freelancersami123@gmail.com>
2025-08-26 07:11:31 +0600

    Leetcode 200. Number of Islands

--- main.cpp
+++ main.cpp
@@ -1,53 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 class Solution {
 public:
     int cnt, n, m;
-    int max = INT_MIN;
-    bool vis[55][55];
+    vector<vector<bool>> vis;
     vector<pair<int,int>> mv = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
     bool valid(int i, int j) {
         return !(i < 0 || i >= n || j < 0 || j >= m);
     }
 
-    void dfs(int si, int sj, vector<vector<int>>& grid) {
-        cnt++;
+    void dfs(int si, int sj, vector<vector<char>>& grid) {
         vis[si][sj] = true;
 
         for(pair mvPair : mv) {
             int ci = si + mvPair.first;
             int cj = sj + mvPair.second;
 
-            if (valid(ci, cj) && grid[ci][cj] == 1 && !vis[ci][cj])
+            if (valid(ci, cj) && grid[ci][cj] == '1' && !vis[ci][cj])
                 {
                     dfs(ci, cj, grid);
                 }
         }
     }
 
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int numIslands(vector<vector<char>>& grid) {
         cnt = 0;
         n = grid.size();
         m = grid[0].size();
-        memset(vis, false, sizeof(vis));
+        vis.assign(n, vector<bool>(m, false));
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
-                if(!vis[i][j] && grid[i][j] == 1)
+                if(!vis[i][j] && grid[i][j] == '1')
                     {
+                        cnt++;
                         dfs(i, j, grid);
-                        if(max < cnt) {
-                            max=cnt;
-                        }
-                        cnt=0;
                     }
 
-        if (max <= 0) {
-            return 0;
-        }else {
-            return max;
-        }
+        return cnt;
     }
 };
