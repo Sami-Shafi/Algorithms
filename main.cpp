e5c487812122c3b2fa03a69ee078f5d658a502dc
Sami-Shafi <freelancersami123@gmail.com>
2025-08-31 06:35:16 +0600

    area of components in 2d grid hackerrank

--- main.cpp
+++ main.cpp
@@ -3,40 +3,25 @@ using namespace std;
 
 vector<vector<char>> grid;
 vector<vector<bool>> vis;
-vector<vector<int>> level;
+// vector<vector<int>> level;
 vector<pair<int,int>> mv = {{-1,0}, {1,0}, {0, -1}, {0, 1}};
-int n, m;
+int n, m, cnt = 0, minArea = INT_MAX;
 
 bool valid(int i, int j) {
-    if (i<0 || i>=n || j<0 || j>=m || grid[i][j] == '-') return false;
-    return true;
+    return !(i<0 || i>=n || j<0 || j>=m || grid[i][j] == '-');
 }
 
-void sameComp(int si, int sj, int di, int dj) {
-    if(!valid(si, sj) || !valid(di, dj)) return;
-    queue<pair<int, int>> q;
-    q.push({si, sj});
+void dfs(int si, int sj, vector<vector<char>>& grid) {
+    cnt++;
     vis[si][sj] = true;
-    level[si][sj] = 0;
 
-    while (!q.empty())
-    {
-        pair<int, int> par = q.front();
-        q.pop();
+    for(auto mvPair : mv) {
+        int ci = si + mvPair.first;
+        int cj = sj + mvPair.second;
 
-        for (int i = 0; i < mv.size(); i++)
-        {
-            int ci = par.first + mv[i].first;
-            int cj = par.second + mv[i].second;
-
-            if(valid(ci, cj) && !vis[ci][cj])
-            {
-                q.push({ci, cj});
-                vis[ci][cj] = true;
-                level[ci][cj] = level[par.first][par.second] + 1;
-            }
+        if (valid(ci, cj) && !vis[ci][cj]) {
+            dfs(ci, cj, grid);
         }
-        
     }
 }
 
@@ -45,8 +30,7 @@ int main() {
     cin >> n >> m;
     grid.assign(n, vector<char>(m, ' '));
     vis.assign(n, vector<bool>(m, false));
-    level.assign(n, vector<int>(m, -1));
-    int si,sj,di,dj;
+    // level.assign(n, vector<int>(m, -1));
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
@@ -56,14 +40,19 @@ int main() {
                 grid[i][j] = ourChar;
             }
 
-    cin >> si >> sj >> di >> dj;
-    sameComp(si, sj, di, dj);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if(grid[i][j] == '.' && !vis[i][j]) {
+                dfs(i, j, grid);
+                if(cnt < minArea)
+                    minArea = cnt;
+                cnt = 0;
+            }
 
-    if(level[di][dj] == -1){
-        cout << "NO";
-    }else {
-        cout << "YES";
-    }
-
+    if(minArea==INT_MAX)
+        cout << -1;
+    else 
+        cout << minArea;
+    
     return 0;
 }
