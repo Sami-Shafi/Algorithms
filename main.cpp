a9991db6fd34b2a2f2fccff3e5846b82ab0eb1bd
Sami-Shafi <freelancersami123@gmail.com>
2025-08-31 09:01:33 +0600

    Shortest Path Crosser in 2d grid

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// You will be given a chessboard of NxM size. You can move anywhere in the chessboard freely. You will be given two cells - the knight's cell K(Ki and Kj), and the queen's cell Q(Qi and Qj). You need to tell the minimum number of steps for the knight to attack the queen if the queen doesn't move. A knight move in 8 directions. Output the minimum number of steps for the knight to reach the queen. If you can't reach to queen, print -1.
+// Rezia is trapped in a 2D maze of NxM size, starting at position R, and her goal is to reach the exit marked by D. The maze contains blocks represented by #, and she can only traverse through cells marked with dots (.). As she need to escape as early as possible, we need to determine the path she will follow.
 
-// First line will contain T, the number of test cases.
-// First line of each test case will contain N and M.
-// Second line of each test case will contain Ki and Kj.
-// Third line of each test case will contain Qi and Qj.
+// Place an X in each cell representing Rezia's route to exit the maze. If there is no viable path for her to exit, leave the maze unchanged.
+
+// Note: Rezia can move in four directions – right, left, up, and down. It is crucial to adhere to the specified order: attempting right first, then left, followed by up, and finally down.
 
 vector<vector<char>> grid;
 vector<vector<bool>> vis;
-vector<vector<int>> level;
-vector<pair<int,int>> mv = {{-1,-2}, {-2,-1}, {-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}};
+vector<pair<int,int>> mv = {{0,1}, {0,-1}, {-1,0}, {1,0}};
+vector<vector<pair<int,int>>> parent;
 int n, m;
 int si,sj,di,dj;
 
 bool valid(int i, int j) {
-    return !(i<0 || i>=n || j<0 || j>=m);
+    return !(i<0 || i>=n || j<0 || j>=m || grid[i][j] == '#');
 }
 
-int shortestDis(int si, int sj, int di, int dj) {
-    if(!valid(si, sj) || !valid(di, dj)) return -1;
+void shortestDis(int si, int sj, int di, int dj) {
+    if(!valid(si, sj) || !valid(di, dj)) return;
     queue<pair<int, int>> q;
     q.push({si, sj});
     vis[si][sj] = true;
-    level[si][sj] = 0;
 
+    bool found = false;
     while (!q.empty())
     {
         pair<int, int> par = q.front();
@@ -34,7 +33,10 @@ int shortestDis(int si, int sj, int di, int dj) {
         int p_i = par.first;
         int p_j = par.second;
 
-        if(p_i == di && p_j == dj) return level[p_i][p_j];
+        if (p_i == di && p_j == dj){
+            found = true;
+            break;
+        }
 
         for (auto mvPair : mv)
         {
@@ -45,32 +47,57 @@ int shortestDis(int si, int sj, int di, int dj) {
             {
                 q.push({ci, cj});
                 vis[ci][cj] = true;
-                level[ci][cj] = level[par.first][par.second] + 1;
+                parent[ci][cj] = {p_i, p_j};
             }
         }
-        
     }
-    return -1;
+
+    if(!found) return;
+    int xi = di, xj = dj;
+    while (grid[xi][xj] != 'R')
+    {
+        int par_i = parent[xi][xj].first;
+        int par_j = parent[xi][xj].second;
+
+        if(par_i == -1 || par_j == -1) break;
+        if(grid[xi][xj] != 'R' && grid[xi][xj] != 'D')
+            grid[xi][xj] = 'X';
+
+        xi = par_i;
+        xj = par_j;
+    }
+    
+
 }
 
 int main() {
+    cin >> n >> m;
+    grid.assign(n, vector<char>(m, '.'));
+    vis.assign(n, vector<bool>(m, false));
+    parent.assign(n, vector<pair<int, int>>(m, {-1, -1}));
 
-    int t;
-    cin >> t;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            {
+                char ourChar;
+                cin >> ourChar;
+                grid[i][j] = ourChar;
 
-    while (t--)
+                if(ourChar == 'R'){
+                    si = i;
+                    sj = j;
+                }else if(ourChar == 'D'){
+                    di = i;
+                    dj = j;
+                }
+            }
+
+    shortestDis(si, sj, di, dj);
+    for (int i = 0; i < n; i++)
     {
-        cin >> n >> m;
-        grid.assign(n, vector<char>(m, '.'));
-        vis.assign(n, vector<bool>(m, false));
-        level.assign(n, vector<int>(m, -1));
-
-        cin >> si >> sj >> di >> dj;
-        if(si == di && sj == dj) {
-            cout << 0 << endl;
-            continue;
-        }
-        cout << shortestDis(si, sj, di, dj) << endl;
+        for (int j = 0; j < m; j++)
+            cout << grid[i][j];
+        cout << endl;
     }
     
     return 0;
