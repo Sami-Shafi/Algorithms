1d7baa5c315d69ac3fa5441e2e060f7cfb307146
Sami-Shafi <freelancersami123@gmail.com>
2025-09-05 01:59:48 +0600

    DSU with recursion

--- main.cpp
+++ main.cpp
@@ -1,78 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+int leads[1005];
 
-// There are n cities and m roads between them. Your task is to process q queries where you have to determine the length of the shortest route between two given cities.
-// Input
-// The first input line has three integers n, m and q: the number of cities, roads, and queries.
-// Then, there are m lines describing the roads. Each line has three integers a, b and c: there is a road between cities a and b whose length is c. All roads are two-way roads.
-// Finally, there are q lines describing the queries. Each line has two integers a and b: determine the length of the shortest route between cities a and b.
-// Output
-// Print the length of the shortest route for each query. If there is no route, print -1 instead.
+// loop
+// int find (int node)
+// {
+//     while (leads[node] != -1)
+//     {
+//         node = leads[node];
+//     }
+//     return node;
+// }
 
-// Example
-// Input:
-// 4 3 5
-// 1 2 5
-// 1 3 9
-// 2 3 3
-// 1 2
-// 2 1
-// 1 3
-// 1 4
-// 3 2
+// recursion
+int find (int node)
+{
+    if(leads[node] == -1)
+        return node;
 
-// Output:
-// 5
-// 5
-// 8
-// -1
-// 3
-
-int n,m,q;
-vector<vector<long long int>> adj_mat;
-
-void floydHarshall() {
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-            if (adj_mat[i][k] != LLONG_MAX && adj_mat[k][j] != LLONG_MAX)
-                    adj_mat[i][j] = min(adj_mat[i][j], adj_mat[i][k] + adj_mat[k][j]);
+    int newleads = find(leads[node]);
+    leads[node] = newleads;
+    return newleads;
 }
 
-
 int main() {
+    memset(leads, -1, sizeof(leads));
+    leads[0] = 1;
+    leads[1] = -1;
+    leads[2] = 7;
+    leads[3] = 1;
+    leads[4] = 5;
+    leads[5] = 3;
 
-    cin >> n >> m >> q;
-    adj_mat.assign(n+5, vector<long long int>(n+5, 0));
+    cout << find(4) << endl;
 
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if(i!=j) {
-                adj_mat[i][j] = LLONG_MAX;
-            }
-
-    while (m--)
+    for (int i = 0; i < 6; i++)
     {
-        long long int a,b,c;
-        cin >> a >> b >> c;
-
-        int minWeight = min(adj_mat[a][b], c);
-        adj_mat[a][b] = minWeight;
-        adj_mat[b][a] = minWeight;
+        cout << i << " -> " << leads[i] << endl;
     }
     
-    floydHarshall();
-
-    while (q--)
-    {
-        long long int src, target;
-        cin >> src >> target;
-
-        if(adj_mat[src][target] == LLONG_MAX)
-            cout << -1 << endl;
-        else
-            cout << adj_mat[src][target] << endl;
-    }
 
     return 0;
 }
