bb8bb7d75a514eb7f16aa967ba5d7233df2a2d15
Sami-Shafi <freelancersami123@gmail.com>
2025-09-07 05:58:45 +0600

    DSU Cycle counter problem solve

--- main.cpp
+++ main.cpp
@@ -2,61 +2,54 @@
 using namespace std;
 
 int n,e;
-vector<vector<long long int>> adj_mat;
+vector<int> leads;
+vector<int> group_size;
 
-// You'll be given a graph of N nodes and E edges. For each edge, you'll be given A, B and W which means there is an edge from A to B only and which will cost W.
-
-// Also, you'll be given Q queries, for each query you'll be given S and D, where S is the source and D is the destination. You need to print the minimum cost from S to D for each query. If there is no connection between S and D, print -1.
-
-// Note: There can be multiple edges from one node to another. Make sure you handle this one.
+// You will be given an undirected graph where there will be N nodes and E edges. You need to tell the number of edges that can create a cycle in the graph. Note: Duplicate edges as input can not be possible. The value of nodes are from 1 to N.
 
 // Input Format
-
 // First line will contain N and E.
-// Next E lines will contain A, B and W.
-// After that you'll get Q.
-// Next Q queries will contain S and D.
+// Next lines will contain A and B which means there is a edge between A and B.
 
-void floydWarshall () {
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                if(adj_mat[i][k] != LLONG_MAX && adj_mat[k][j] != LLONG_MAX)
-                    adj_mat[i][j] = min(adj_mat[i][k]+adj_mat[k][j], adj_mat[i][j]);
-    
+int find (int node)
+{
+    if(leads[node] == -1)
+        return node;
+
+    leads[node] = find(leads[node]);
+    return leads[node];
+}
+
+void dsu_union(int lead1, int lead2) {
+    if(group_size[lead1] >= group_size[lead2]) {
+        leads[lead2] = lead1;
+        group_size[lead1]+=group_size[lead2];
+    }else {
+        leads[lead1] = lead2;
+        group_size[lead2]+=group_size[lead1];
+    }
 }
 
 int main() {
     
     cin >> n >> e;
-    adj_mat.assign(n+5, vector<long long int>(n+5, LLONG_MAX));
+    leads.assign(n+5, -1);
+    group_size.assign(n+5, 1);
 
-    for (int i = 1; i <= n; i++)
-        adj_mat[i][i] = 0;
-
+    int cycle = 0;
     while (e--)
     {
         int a,b;
-        long long int w;
-        cin >> a >> b >> w;
-        adj_mat[a][b] = min(adj_mat[a][b], w);
-    }
-    
-    floydWarshall();
-
-    int q;
-    cin >> q;
-    while (q--)
-    {
-        int s,d;
-        cin >> s >> d;
-
-        if(adj_mat[s][d] != LLONG_MAX)
-            cout << adj_mat[s][d] << endl;
+        cin >> a >> b;
+        int lead1 = find(a);
+        int lead2 = find(b);
+        if(lead1 == lead2)
+            cycle++;
         else
-            cout << -1 << endl;
+            dsu_union(lead1, lead2);
     }
-    
+
+    cout << cycle << endl;
 
     return 0;
 }
