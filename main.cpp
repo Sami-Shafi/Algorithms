496a2df9598e60b9a59cb4e7eb44d8d75bcc0345
Sami-Shafi <freelancersami123@gmail.com>
2025-09-11 04:05:36 +0600

    Fibonacci Recursion Unoptimized Example

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int n) {
-    if (n < 1) return 0;
-    return sum(n-1) + n;
+int fibo(int n) {
+    if (n < 2) return n;
+    return fibo(n-1) + fibo(n-2);
 }
 
 int main() {
 
     int n;
-    cout << "Input Number to get sum till 1" << endl;
     cin >> n;
-    cout << sum(n) << endl;
+    cout << fibo(n) << endl;
 
     return 0;
 }
