33eeda386fb65ba4e458172d3301cacc6c5bf711
Sami-Shafi <freelancersami123@gmail.com>
2025-09-11 03:00:20 +0600

    Strong password Problem solve

--- main.cpp
+++ main.cpp
@@ -1,55 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,e;
-vector<int> leads;
-vector<int> group_size;
-
-// You will be given an undirected graph where there will be N nodes and E edges. You need to tell the number of edges that can create a cycle in the graph. Note: Duplicate edges as input can not be possible. The value of nodes are from 1 to N.
-
-// Input Format
-// First line will contain N and E.
-// Next lines will contain A and B which means there is a edge between A and B.
-
-int find (int node)
-{
-    if(leads[node] == -1)
-        return node;
-
-    leads[node] = find(leads[node]);
-    return leads[node];
-}
-
-void dsu_union(int lead1, int lead2) {
-    if(group_size[lead1] >= group_size[lead2]) {
-        leads[lead2] = lead1;
-        group_size[lead1]+=group_size[lead2];
-    }else {
-        leads[lead1] = lead2;
-        group_size[lead2]+=group_size[lead1];
-    }
-}
-
 int main() {
     
-    cin >> n >> e;
-    leads.assign(n+5, -1);
-    group_size.assign(n+5, 1);
+    int t;
+    cin >> t;
 
-    int cycle = 0;
-    while (e--)
+    while (t--)
     {
-        int a,b;
-        cin >> a >> b;
-        int lead1 = find(a);
-        int lead2 = find(b);
-        if(lead1 == lead2)
-            cycle++;
-        else
-            dsu_union(lead1, lead2);
-    }
+        string word;
+        int insIdx = 0;
+        cin >> word;
+        char dchar;
 
-    cout << cycle << endl;
+        if(word[0] == 'z')
+            dchar = 'y';
+        else
+            dchar = word[0]+1; 
+            
+        for (int i=0; i < word.length(); i++)
+        {
+            if(i != 0)
+                if(word[i-1] == word[i]) {
+                    insIdx = i;
+                    if(word[i] == 'z')
+                        dchar = 'y';
+                    else
+                        dchar = word[i]+1; 
+                    break;
+                }
+
+        }
+        word.insert(word.begin()+insIdx, dchar);
+        cout << word << endl;
+    }
+    
 
     return 0;
 }
