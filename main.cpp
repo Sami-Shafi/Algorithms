536e88a5f8f8030dc89bb087cca201fa74195566
Sami-Shafi <freelancersami123@gmail.com>
2025-09-14 03:26:05 +0600

    KnapSack 0-1 DP

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 vector<int> val;
 vector<int> weight;
+vector<vector<int>> dp;
 
 int knapsack(int idx, int weight_cap) {
     if(idx < 0 || weight_cap == 0)
         return 0;
 
+    if(dp[idx][weight_cap] != -1)
+        return dp[idx][weight_cap];
+
     int opt1 = knapsack(idx-1, weight_cap-weight[idx]) + val[idx];
     int opt2 = knapsack(idx-1, weight_cap);
     if(weight[idx] <= weight_cap)
-        return max(opt1, opt2);
+        return dp[idx][weight_cap] = max(opt1, opt2);
     else
-        return opt2;
+        return dp[idx][weight_cap] = opt2;
 }
 
 int main ()
@@ -22,6 +26,8 @@ int main ()
     cin >> n;
     val.resize(n);
     weight.resize(n);
+
+    dp.assign(n, vector<int>(weight_cap, -1));
 
     for (int i = 0; i < n; i++)
         cin >> val[i];
