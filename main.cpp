84197e72adf6ee7270d073a7e0091c9fcb420218
Sami-Shafi <freelancersami123@gmail.com>
2025-09-14 04:56:08 +0600

    tetranci Bottom Up

--- main.cpp
+++ main.cpp
@@ -1,43 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// You will be given a positive integer N. You will start from 1 and do some steps (possibly zero).
-
-// In each step you can choose one of the following:
-// Add 3 with the current value
-// Multiply by 2 with the current value
-
-// Can you tell if you can reach n by using any number of steps you want.
-
-bool reacher(int n) {
-    if(n < 1)
-        return false;
-    
-    if(n == 1)
-        return true;
-
-    if(n % 2 == 0)
-        return reacher(n/2);
-    else
-        return reacher(n-3);
-}
-
 int main() {
-
-    int t;
-    cin >> t;
-
-    while (t--)
+    
+    long long int n;
+    cin >> n;
+    long long int tetra[n+1];
+    tetra[0] = 0;
+    tetra[1] = 1;
+    tetra[2] = 1;
+    tetra[3] = 2;
+    
+    for (long long int i = 4; i <= n; i++)
     {
-        int n;
-        cin >> n;
-
-        if(reacher(n))
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        tetra[i] = tetra[i-1] + tetra[i-2] + tetra[i-3] + tetra[i-4];
     }
+
+    cout << tetra[n] << endl;
 
     return 0;
 }
